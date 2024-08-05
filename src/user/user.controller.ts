74c5791294ce10c48d98441ed46323c5530027af
Vlad Svidenko <vlad.s@redentu.com>
2024-08-05 15:12:57 +0300

    feat: "Add user"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Put } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorators/auth.decorators';
+import { AuthUser } from 'src/auth/decorators/user.decorators';
+import { UserDto } from './dto/user.dto';
 
-@Controller('user')
+@Auth()
+@Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Get('/current-user/profile')
+	async getProfile(@AuthUser('id') id: string) {
+		return this.userService.getProfile(id);
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Put('/current-user')
+	async updateProfile(@AuthUser('id') id: string, @Body() payload: UserDto) {
+		return this.userService.update(id, payload);
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { hash } from 'argon2';
 import { AuthDto } from 'src/auth/dto/auth.dto';
 import { PrismaService } from 'src/prisma.service';
+import { UserDto } from './dto/user.dto';
+import { startOfDay, subDays } from 'date-fns';
 
 @Injectable()
 export class UserService {
@@ -20,6 +22,45 @@ export class UserService {
 		});
 	}
 
+	public async getProfile(id: string) {
+		const profile = await this.getById(id);
+
+		const totalTasks = profile.tasks.length;
+		const completedTasks = await this.prisma.task.count({
+			where: { userId: id, isCompleted: true }
+		});
+
+		const todayStart = startOfDay(new Date());
+		const weekStart = startOfDay(subDays(new Date(), 7));
+
+		const todayTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: { gte: todayStart.toISOString() }
+			}
+		});
+
+		const weekTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: { gte: weekStart.toISOString() }
+			}
+		});
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...rest } = profile;
+
+		return {
+			user: rest,
+			statistics: [
+				{ label: 'Total', value: totalTasks },
+				{ label: 'Completed tasks', value: completedTasks },
+				{ label: 'Today tasks', value: todayTasks },
+				{ label: 'Week tasks', value: weekTasks }
+			]
+		};
+	}
+
 	public async create(payload: AuthDto) {
 		const user = {
 			email: payload.email,
@@ -28,5 +69,15 @@ export class UserService {
 		};
 
 		return this.prisma.user.create({ data: user });
+	}
+
+	public async update(id: string, payload: UserDto) {
+		let data = payload;
+
+		if (data.password) {
+			data = { ...payload, password: await hash(payload.password) };
+		}
+
+		return this.prisma.user.update({ where: { id: id }, data, select: { name: true, email: true } });
 	}
 }
